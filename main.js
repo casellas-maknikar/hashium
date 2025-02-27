ec9a5961afbb277daedbeba1bfe6414766c01b40
Casellas-Maknikar <holdings@casellas-maknikar.com>
2025-02-26 23:22:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import CarrdRouter from "./libs/carrdRouter.js";
 import CarrdOptimizer from "./libs/carrdOptimizer.js";
 
-const carrdRouter = new CarrdRouter();
 const carrdOptimizer = new CarrdOptimizer();
