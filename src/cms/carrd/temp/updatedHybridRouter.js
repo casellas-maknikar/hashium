1997cb9c81eca469e281290dbb087ab956af427b
Marc A. Casellas <Marc@cyber-wizard.com>
2025-05-31 03:47:26 -0400

    Update updatedHybridRouter.js

--- src/cms/carrd/temp/updatedHybridRouter.js
+++ src/cms/carrd/temp/updatedHybridRouter.js
@@ -1,14 +1,13 @@
-const locRef     = window.location;
-const originRef  = locRef.origin;
+const locRef = window.location;
+const originRef = locRef.origin;
 const replaceState = window.history.replaceState.bind(window.history);
-const addEvent     = window.addEventListener.bind(window);
+const addEvent = window.addEventListener.bind(window);
 
 class HybridRouter {
-  // Private fields hold our “hidden” references
-  #loc   = locRef;
-  #orig  = originRef;
-  #rS    = replaceState;
-  #aEL   = addEvent;
+  #loc = locRef;
+  #orig = originRef;
+  #rS = replaceState;
+  #aEL = addEvent;
 
   constructor() {
     Object.freeze(this);
@@ -26,9 +25,7 @@ class HybridRouter {
 
   init() {
     this.#aEL('load', () => this.route());
-
     this.#aEL('hashchange', () => setTimeout(() => this.route(), 0));
-
     this.#aEL('popstate', e => {
       const section = e.state?.section;
       if (typeof section === 'string') {
